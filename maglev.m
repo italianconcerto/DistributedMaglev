6fff5cf391f7d16674a3a807ab2c2308651b7442
CapacitorSet <capacitorset@gmail.com>
2022-06-15 10:59:10 +0200

    Implement local observer

--- maglev.m
+++ maglev.m
@@ -16,7 +16,7 @@ agent_sys = ss(A, B, C, D);
 K_leader = place(A,B, [0 -20]);
 
 Gr = digraph(1:5, 2:6, [2 6 1 1 3]);
-Adj = full(adjacency(Gr));
+Adj = full(adjacency(Gr, 'weighted'));
 % Adj = zeros(6); % no interconnections
 in_degrees = sum(Adj, 1);
 D_graph = diag(in_degrees);
@@ -28,8 +28,11 @@ lambda_i = eig(L + G);
 
 R = 5; Q = 5*eye(2);
 P = are(A, B*inv(R)*(B'), Q);
-
-K = inv(R)*(B')*P
+K = inv(R)*(B')*P;
 
 min_c = 1/(2*min(real(lambda_i)))
 c = 0.7;
+
+
+P = are(A', C'*inv(R)*C, Q)
+F = P*C'*inv(R);
