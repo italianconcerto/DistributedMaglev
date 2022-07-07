aa67de8b85805c1c92f6af751031b2d5a91055b7
CapacitorSet <capacitorset@gmail.com>
2022-07-08 01:46:40 +0200

    Fix

--- maglev.m
+++ maglev.m
@@ -1,10 +1,15 @@
 A = [0 1; 880.87 0];
 B = [0; -9.9453];
-C = [708.27 0]; C = -C/100;
+% C = [708.27 0] / 1e3;
+C = [1 0];
 D = [0];
 
+% TODO !!!! we can't access the state variables
+% -> need to use an observer that feeds the controller
+K_leader = acker(A, B, [-20 0]);
+A = A - B*K_leader;
+
 agent_sys = ss(A, B, C, D);
-K_leader = place(A,B, [0 -20]);
 Obs_gain = place(A', C', [-3 -5]);
 
 % Debug topology: all nodes are pinned
@@ -36,44 +41,45 @@ P = are(A, B*inv(R)*(B'), Q);
 K = inv(R)*(B')*P;
 
 min_c = 1/(2*min(real(lambda_i)))
-c = 0.51;
+c = 3;
 
 P = are(A', C'*inv(R)*C, Q)
 F = P*C'*inv(R);
 
-c_values = linspace(min_c + 0.1, 3*min_c, 20);
+if 0 == 1
+    c_values = linspace(min_c + 0.1, 3*min_c, 20);
 
-all_rise_times = zeros([20 1]);
-
-i = 20;
-%for i = 1:20
-c = c_values(i)
-
-out = sim('maglev_sim');
-
-rise_times = zeros([6 2]);
-rise_low_thres = 0.1;
-rise_high_thres = 0.9;
-rise_times(1, 1) = out.y1.Time(find(out.y1.Data > rise_low_thres, 1)) - 1;
-rise_times(1, 2) = out.y1.Time(find(out.y1.Data > rise_high_thres, 1)) - 1;
-rise_times(2, 1) = out.y1.Time(find(out.y2.Data > rise_low_thres, 1)) - 1;
-rise_times(2, 2) = out.y1.Time(find(out.y2.Data > rise_high_thres, 1)) - 1;
-rise_times(3, 1) = out.y1.Time(find(out.y3.Data > rise_low_thres, 1)) - 1;
-rise_times(3, 2) = out.y1.Time(find(out.y3.Data > rise_high_thres, 1)) - 1;
-rise_times(4, 1) = out.y1.Time(find(out.y4.Data > rise_low_thres, 1)) - 1;
-rise_times(4, 2) = out.y1.Time(find(out.y4.Data > rise_high_thres, 1)) - 1;
-rise_times(5, 1) = out.y1.Time(find(out.y5.Data > rise_low_thres, 1)) - 1;
-rise_times(5, 2) = out.y1.Time(find(out.y5.Data > rise_high_thres, 1)) - 1;
-rise_times(6, 1) = out.y1.Time(find(out.y6.Data > rise_low_thres, 1)) - 1;
-rise_times(6, 2) = out.y1.Time(find(out.y6.Data > rise_high_thres, 1)) - 1;
-
-leader_rise_times = [0 0];
-leader_rise_times(1) = out.y_leader.Time(find(out.y_leader.Data > rise_low_thres, 1)) - 1;
-leader_rise_times(2) = out.y_leader.Time(find(out.y_leader.Data > rise_high_thres, 1)) - 1;
-
-all_rise_times(i) = rise_times(6, 2);
-rise_times(6, 2)
-%end
-
-%plot(c_values, all_rise_times)
-
+    all_rise_times = zeros([20 1]);
+    
+    i = 20;
+    %for i = 1:20
+    c = c_values(i)
+    
+    out = sim('maglev_sim');
+    
+    rise_times = zeros([6 2]);
+    rise_low_thres = 0.1;
+    rise_high_thres = 0.9;
+    rise_times(1, 1) = out.y1.Time(find(out.y1.Data > rise_low_thres, 1)) - 1;
+    rise_times(1, 2) = out.y1.Time(find(out.y1.Data > rise_high_thres, 1)) - 1;
+    rise_times(2, 1) = out.y1.Time(find(out.y2.Data > rise_low_thres, 1)) - 1;
+    rise_times(2, 2) = out.y1.Time(find(out.y2.Data > rise_high_thres, 1)) - 1;
+    rise_times(3, 1) = out.y1.Time(find(out.y3.Data > rise_low_thres, 1)) - 1;
+    rise_times(3, 2) = out.y1.Time(find(out.y3.Data > rise_high_thres, 1)) - 1;
+    rise_times(4, 1) = out.y1.Time(find(out.y4.Data > rise_low_thres, 1)) - 1;
+    rise_times(4, 2) = out.y1.Time(find(out.y4.Data > rise_high_thres, 1)) - 1;
+    rise_times(5, 1) = out.y1.Time(find(out.y5.Data > rise_low_thres, 1)) - 1;
+    rise_times(5, 2) = out.y1.Time(find(out.y5.Data > rise_high_thres, 1)) - 1;
+    rise_times(6, 1) = out.y1.Time(find(out.y6.Data > rise_low_thres, 1)) - 1;
+    rise_times(6, 2) = out.y1.Time(find(out.y6.Data > rise_high_thres, 1)) - 1;
+    
+    leader_rise_times = [0 0];
+    leader_rise_times(1) = out.y_leader.Time(find(out.y_leader.Data > rise_low_thres, 1)) - 1;
+    leader_rise_times(2) = out.y_leader.Time(find(out.y_leader.Data > rise_high_thres, 1)) - 1;
+    
+    all_rise_times(i) = rise_times(6, 2);
+    rise_times(6, 2)
+    %end
+    
+    %plot(c_values, all_rise_times)
+end
